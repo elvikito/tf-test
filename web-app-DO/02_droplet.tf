8af38ffb409dc68cea681fd0d96416941f7ad9a7
Elvis Ramirez <elvikito@gmail.com>
2022-07-28 14:47:47 -0400

    Modificando el tipo

--- web-app-DO/02_droplet.tf
+++ web-app-DO/02_droplet.tf
@@ -2,6 +2,6 @@ resource "digitalocean_droplet" "web" {
   image     = "ubuntu-18-04-x64"
   name      = "web-1"
   region    = "nyc1"
-  size      = "s-1vcpu-1gb"
+  size      = "s-1vcpu-512mb-10gb"
   ssh_keys  = ["${digitalocean_ssh_key.eramirez.fingerprint}"]
 }
